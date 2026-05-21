88ecb90289a147a5f14a75ce66892a1705bbebf9
aadharsjbrana101 <aadharsjbrana@gmail.com>
2026-05-21 15:26:35 +0545

    Fixed gitignore

--- src/Model/userdata.java
+++ src/Model/userdata.java
@@ -4,10 +4,41 @@
  */
 package Model;
 
-/**
- *
- * @author aadha
- */
 public class userdata {
-    
+    private String username;
+    private String email;
+    private String password;
+
+    // Constructor
+    public userdata(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters (useful later for login/forgot password)
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -3,13 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+import Database.mysqlconnection;
+import Model.userdata;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
-/**
- *
- * @author aadha
- */
 public class UserDao {
     mysqlconnection mysql = new mysqlconnection();
-    public void createuser()
-    
-}
+
+    public void createUser(userdata user) { 
+        Connection conn = mysql.openconnection();
+        String sql = "INSERT INTO users (username, email, password) VALUES (?,?,?)";
+        try (PreparedStatement pstm = conn.prepareStatement(sql)) {  
+            pstm.setString(1, user.getUsername());   
+            pstm.setString(2, user.getEmail());
+            pstm.setString(3, user.getPassword());
+            pstm.executeUpdate();                    
+        } catch (Exception e) {
+            System.out.print(e);                     
+        } finally {
+            mysql.closeconnection(conn);
+        }
+    }  
+}    
